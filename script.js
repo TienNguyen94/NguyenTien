01ea4cd5094c3c3c5fd697933033e23fb8922a3e
Nguyễn Đình Tiến <kakashisenise1999@gmail.com>
2022-03-03 11:33:21 +0900

    upload source code

---
+++ script.js
@@ -0,0 +1,96 @@
+/*-- Toggle Navbar -- */
+const navToggle = document.querySelector('.nav-toggler')
+navToggle.addEventListener('click', () => {
+  hideSections()
+  toggleNavbar()
+  document.body.classList.toggle('hide-scrolling')
+})
+
+function hideSections () {
+  document.querySelector('section.active').classList.toggle('fade-out')
+}
+function toggleNavbar () {
+  document.querySelector('header').classList.toggle('active')
+}
+
+/*-- Active Section -- */
+document.addEventListener('click', e => {
+  if (e.target.classList.contains('link-item') && e.target.hash !== '') {
+    navToggle.classList.add('hide')
+
+    // Active the overlay to prevent multiple clicks
+    // Nói thẳng ra là tránh mấy thằng hãm spam cái nút đó!!!
+    document.querySelector('.overlay').classList.add('active')
+
+    if (e.target.classList.contains('nav-item')) {
+      toggleNavbar()
+    } else {
+      hideSections()
+      document.body.classList.add('hide-scrolling')
+    }
+    setTimeout(() => {
+      document
+        .querySelector('section.active')
+        .classList.remove('active', 'fade-out')
+      document.querySelector(e.target.hash).classList.add('active')
+      window.scrollTo(0, 0)
+      document.body.classList.remove('hide-scrolling')
+      navToggle.classList.remove('hide')
+      document.querySelector('.overlay').classList.remove('active')
+    }, 500)
+  }
+})
+
+/*-- About Tabs -- */
+const tabsContainer = document.querySelector('.about-tabs'),
+  aboutSection = document.querySelector('.about-section')
+
+tabsContainer.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('tab-item') &&
+    !e.target.classList.contains('active')
+  ) {
+    tabsContainer.querySelector('.active').classList.remove('active')
+    e.target.classList.add('active')
+    const target = e.target.getAttribute('data-target')
+    aboutSection.querySelector('.tab-content.active').classList.remove('active')
+    aboutSection.querySelector(target).classList.add('active')
+  }
+})
+/*-- Portfolio Item Details Popup -- */
+document.addEventListener('click', e => {
+  if (e.target.classList.contains('view-project-btn')) {
+    togglePortfolioPopup()
+    document.querySelector('.portfolio-popup').scrollTo(0, 0)
+    portfolioItemDetails(e.target.parentElement)
+  }
+})
+function togglePortfolioPopup () {
+  document.querySelector('.portfolio-popup').classList.toggle('open')
+  document.body.classList.toggle('hide-scrolling')
+  document.querySelector('.main').classList.toggle('fade-out')
+}
+document
+  .querySelector('.pp-close')
+  .addEventListener('click', togglePortfolioPopup)
+
+/*-- Hide popup when clicking outside of item --*/
+document.addEventListener('click', e => {
+  if (e.target.classList.contains('pp-inner')) {
+    togglePortfolioPopup()
+  }
+})
+
+function portfolioItemDetails (portfolioItem) {
+  document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector(
+    '.portfolio-item-thumbnail img'
+  ).src
+
+  document.querySelector(
+    '.pp-header h3'
+  ).innerHTML = portfolioItem.querySelector('.portfolio-item-title').innerHTML
+
+  document.querySelector('.pp-body').innerHTML = portfolioItem.querySelector(
+    '.portfolio-item-details'
+  ).innerHTML
+}
