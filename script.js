6ab090415122f02a855f0864c8ac50ef73b07f65
Nguyễn Đình Tiến <67676147+TienNguyen94@users.noreply.github.com>
2022-03-09 02:25:20 +0900

    Latest

--- script.js
+++ script.js
@@ -98,9 +98,34 @@ function portfolioItemDetails (portfolioItem) {
 
   document.querySelector(
     '.pp-header h3'
-  ).innerHTML = portfolioItem.querySelector('.portfolio-item-title').innerHTML
-
+    ).innerHTML = portfolioItem.querySelector('.portfolio-item-title').innerHTML
+    
   document.querySelector('.pp-body').innerHTML = portfolioItem.querySelector(
     '.portfolio-item-details'
-  ).innerHTML
+    ).innerHTML
 }
+
+/*-- Calculator Age  --*/
+function age() {  
+  var d1 = 12;  
+  var m1 = 11;  
+  var y1 = 1999;  
+  var date = new Date();  
+  var d2 = date.getDate();  
+  var m2 = 1 + date.getMonth();  
+  var y2 = date.getFullYear();  
+  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];  
+  if(d1 > d2){  
+   d2 = d2 + month[m2 - 1];  
+   m2 = m2 - 1;  
+  }  
+  if(m1 > m2){  
+   m2 = m2 + 12;  
+   y2 = y2 - 1;  
+  }  
+  var d = d2 - d1;  
+  var m = m2 - m1;  
+  var y = y2 - y1;  
+  let a = document.getElementById('age').innerHTML = y;
+ }
+ age();
