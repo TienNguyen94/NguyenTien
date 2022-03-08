eb4979e25624a4ff054a33f16830de65e7007c3b
Nguyễn Đình Tiến <67676147+TienNguyen94@users.noreply.github.com>
2022-03-09 02:31:39 +0900

    Latest

--- script.js
+++ script.js
@@ -7,7 +7,30 @@ window.addEventListener("load", () =>{
       document.querySelector(".page-loader").style.display = "none";
   }, 600);
 })
-
+/*-- Calculator Age  --*/
+function age() {  
+  let d1 = 12;  
+  let m1 = 11;  
+  let y1 = 1999;  
+  let date = new Date();  
+  let d2 = date.getDate();  
+  let m2 = 1 + date.getMonth();  
+  let y2 = date.getFullYear();  
+  let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];  
+  if(d1 > d2){  
+   d2 = d2 + month[m2 - 1];  
+   m2 = m2 - 1;  
+  }  
+  if(m1 > m2){  
+   m2 = m2 + 12;  
+   y2 = y2 - 1;  
+  }  
+  var d = d2 - d1;  
+  var m = m2 - m1;  
+  var y = y2 - y1;  
+  document.getElementById('age').innerHTML = y;
+ }
+ age();
 /*-- Toggle Navbar -- */
 const navToggle = document.querySelector('.nav-toggler')
 navToggle.addEventListener('click', () => {
@@ -105,27 +128,3 @@ function portfolioItemDetails (portfolioItem) {
     ).innerHTML
 }
 
-/*-- Calculator Age  --*/
-function age() {  
-  var d1 = 12;  
-  var m1 = 11;  
-  var y1 = 1999;  
-  var date = new Date();  
-  var d2 = date.getDate();  
-  var m2 = 1 + date.getMonth();  
-  var y2 = date.getFullYear();  
-  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];  
-  if(d1 > d2){  
-   d2 = d2 + month[m2 - 1];  
-   m2 = m2 - 1;  
-  }  
-  if(m1 > m2){  
-   m2 = m2 + 12;  
-   y2 = y2 - 1;  
-  }  
-  var d = d2 - d1;  
-  var m = m2 - m1;  
-  var y = y2 - y1;  
-  let a = document.getElementById('age').innerHTML = y;
- }
- age();
